826db0fde1965cf13e9ff8197af87d43b2935170
Medvedeva <catherinemedvedeff@gmail.com>
2020-12-14 22:21:59 +0300

    module1-task3

---
+++ src/service/cli/constants.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const CountRequirements = {
+  DEFAULT: 1,
+  MAX: 1000,
+  MAX_MESSAGE: `Не больше 1000 объявлений`,
+};
+
+const MONTH_INTERVAL = 3;
+
+const FILE_NAME = `mocks.json`;
+
+module.exports = {CountRequirements, MONTH_INTERVAL, FILE_NAME};
--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {Cli} = require(`./cli`);
+
+const DEFAULT_COMMAND = `--help`;
+
+const userArguments = process.argv.slice(2);
+const [userCommand] = userArguments;
+
+process.on(`uncaughtException`, (err) => {
+  console.error(err);
+  process.exit(1);
+});
+
+if (userArguments.length === 0 || !Cli[userCommand]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit();
+}
+
+Cli[userCommand].run(userArguments.slice(1));

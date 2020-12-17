1da7f26f2de9dacf9cf6fb53c13686f3a6353d67
Medvedeva <catherinemedvedeff@gmail.com>
2020-12-17 16:43:42 +0300

    module2-task3

--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -12,6 +12,7 @@ const HELP_TEXT = `
     --version:            выводит номер версии
     --help:               печатает этот текст
     --generate <count>    формирует файл mocks.json
+    --server <port>       запускает сервер
 `;
 
 module.exports = {
